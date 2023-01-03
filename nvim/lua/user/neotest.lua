3a0ed07c0eebbfe72d8bfda80bc91cf38869bc17
Roman Melnyk <roman.melnyk20071992@gmail.com>
2023-01-04 00:11:39 +0200

    coverage arg to neotest

--- nvim/lua/user/neotest.lua
+++ nvim/lua/user/neotest.lua
@@ -2,6 +2,17 @@ local status_ok, neotest = pcall(require, "neotest")
 if not status_ok then
   return
 end
+-- get neotest namespace (api call creates or returns namespace)
+-- local neotest_ns = vim.api.nvim_create_namespace("neotest")
+-- vim.diagnostic.config({
+--   virtual_text = {
+--     format = function(diagnostic)
+--       local message =
+--         diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+--       return message
+--     end,
+--   },
+-- }, neotest_ns)
 
 neotest.setup({
   adapters = {
@@ -14,7 +25,7 @@ neotest.setup({
       experimental = {
         test_table = true,
       },
-      args = { "-count=1", "-timeout=60s" }
+      args = { "-count=1", "-timeout=60s", "-coverprofile=coverage.out" }
     }),
     require("neotest-vim-test")({
       ignore_file_types = { "python", "go", "vim", "lua" },
