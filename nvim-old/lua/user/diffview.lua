0ae319667babb74d8235951e4502e930cd58fbdd
Roman Melnyk <roman.melnyk20071992@gmail.com>
2024-10-02 22:31:47 +0200

    remove old nvim folder

--- nvim-old/lua/user/diffview.lua
+++
@@ -1,8 +0,0 @@
-local diffview_status_ok, diffview = pcall(require, "diffview")
-if not diffview_status_ok then
-  return
-end
-
-diffview.setup({
-    enhanced_diff_hl = true,
-  })
--- nvim-old/lua/user/neodev.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, neodev = pcall(require, "neodev")
-if not status_ok then
-  return
-end
-
-neodev.setup({
-  -- add any options here, or leave empty to use the default settings
-  library = { plugins = { "neotest" }, types = true },
-})
