a3e755a22f67463d1202c378ecb8c4c63219dda2
Roman Melnyk <roman.melnyk20071992@gmail.com>
2024-10-03 14:56:00 +0200

    add codeium plugin

--- nvim/lua/plugins/myplugins.lua
+++ nvim/lua/plugins/myplugins.lua
@@ -167,6 +167,19 @@ local plugins = {
       require "configs.coverage"
     end,
   },
+  {
+    'Exafunction/codeium.vim',
+    event = 'BufEnter',
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set('i', '<C-l>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<C-]>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<C-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    end
+  }
   -- { "mfussenegger/nvim-dap-python" },
 
   -- To make a plugin not be loaded
